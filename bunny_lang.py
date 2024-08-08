625e1324f9c3d053344eb0996241d631568e9ddf
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2024-08-08 17:40:26 +0530

    Update bunny_lang.py

--- bunny_lang.py
+++ bunny_lang.py
@@ -1,12 +1,20 @@
 import streamlit as st
-import mysql.connector
+'''import mysql.connector
 con = mysql.connector.connect(
-            host='localhost',
+            host='127.0.0.1',
             user='root',
             passwd='bunny',
             auth_plugin='mysql_native_password',
             database='secretkeys'
+            
 )
+if con.is_connected():
+    st.text("Connected")'''
+import streamlit as st
+
+# Initialize connection.
+con = st.connection('mysql', type='sql')
+
 cor=con.cursor()
 cor.execute("select * from secret;")
 r=cor.fetchone()
