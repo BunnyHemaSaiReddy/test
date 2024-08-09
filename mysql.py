e381703766976e166b2e23b3c7638d6b13581434
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2024-08-09 14:27:41 +0530

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,20 +1,31 @@
 import streamlit as st
-import mysql.connector
-con = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            passwd='bunny',
-            auth_plugin='mysql_native_password',
-            database='secretkeys'
-            
+import MySQLdb
+
+# Establishing a connection to the MySQL database
+con = MySQLdb.connect(
+    host='localhost',
+    user='root',
+    passwd='bunny',
+    db='secretkeys'
 )
 
-cor=con.cursor()
-cor.execute("select * from secret;")
-r=cor.fetchone()
+# Creating a cursor object to interact with the database
+cor = con.cursor()
+
+# Executing a query to fetch data from the "secret" table
+cor.execute("SELECT * FROM secret;")
+
+# Fetching one record from the result set
+r = cor.fetchone()
+
+# Closing the database connection
 con.close()
+
+# Displaying the fetched data using Streamlit
 st.write(r)
-api_key=r[0]
-access_id= r[1]
-app_pass=r[2]
-app_email=r[3]
+
+# Assigning the fetched values to variables
+api_key = r[0]
+access_id = r[1]
+app_pass = r[2]
+app_email = r[3]
